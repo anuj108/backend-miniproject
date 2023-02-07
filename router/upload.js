ad5d6ce3dece25edb46921b6c5aa5067ebb3230e
AnUj <shakya.anuj910@gmail.com>
2023-02-08 01:09:00 +0530

    fix

--- router/upload.js
+++ router/upload.js
@@ -1,11 +1,12 @@
 const upload = require("../middleware/upload");
 const express = require("express");
 const router = express.Router();
+const BASE_URL = process.env.BASE_URL;
 
 router.post("/upload", upload.single("file"), async (req, res) => {
-    if (req.file === undefined) return res.send("you must select a file.");
-    const imgUrl = `http://localhost:1337/file/${req.file.filename}`;
-    return res.send(imgUrl);
+  if (req.file === undefined) return res.send("you must select a file.");
+  const imgUrl = `${BASE_URL}/file/${req.file.filename}`;
+  return res.send(imgUrl);
 });
 
 module.exports = router;
