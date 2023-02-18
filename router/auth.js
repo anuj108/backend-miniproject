486232413c46ba706bec8d8c8674d847716b9d0c
AnUj <shakya.anuj910@gmail.com>
2023-02-18 18:12:27 +0530

    abc

--- router/auth.js
+++ router/auth.js
@@ -108,6 +108,7 @@ router.post("/login", async (req, res) => {
       res.cookie("jwtoken", token, {
         expires: new Date(Date.now() + 25892000000),
         httpOnly: true,
+        sameSite:'none',
       });
 
       // res.use(cookieSession({
